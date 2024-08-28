233faeb0bfdaa681dbcfce8ef92040d97fa17048
Michael Burt <000947638@mohawkcollege.ca>
2024-08-27 23:47:01 -0400

    added module 2

---
+++ module-2/Game.cs
@@ -0,0 +1,30 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module_2
+{
+    internal static class Game
+    {
+        // If you need variables in the Program class (outside functions), you must mark them as static
+        public static string Title = "Game Title"; // Window title
+        public static int ScreenWidth = 800; // Screen width
+        public static int ScreenHeight = 800; // Screen height
+        public static int TargetFps = 60; // Target frames-per-second
+
+        public static void Setup()
+        {
+            // Game setup code goes here
+        }
+
+        public static void Update()
+        {
+            // Code that runs on every frame goes here
+
+            Raylib.DrawCircle(100, 100, 25, Color.Red);
+        }
+    }
+}
---
+++ module-2/MohawkGameWrapper.cs
@@ -0,0 +1,41 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module_2
+{
+    public static class MohawkGameWrapper
+    {
+        public static void RunGame()
+        {
+            // Create a window to draw to. The arguments define width and height
+            Raylib.InitWindow(Game.ScreenWidth, Game.ScreenHeight, Game.Title);
+
+            // Set the target frames-per-second (FPS)
+            Raylib.SetTargetFPS(Game.TargetFps);
+
+            // Setup your game. This is a function YOU define.
+            Game.Setup();
+
+            // Loop so long as window should not close
+            while (!Raylib.WindowShouldClose())
+            {
+                // Enable drawing to the canvas (window)
+                Raylib.BeginDrawing();
+                // Clear the canvas with one color
+                Raylib.ClearBackground(Color.RayWhite);
+                // Your game code here. This is a function YOU define.
+                Game.Update();
+                // Stop drawing to the canvas, begin displaying the frame
+                Raylib.EndDrawing();
+            }
+            // Close the window
+            Raylib.CloseWindow();
+        }
+
+    }
+}
