277b9e79bc3056d447f3b137f4f9b38e9014ec1b
Michael Burt <000947638@mohawkcollege.ca>
2024-08-27 21:21:00 -0400

    Added module 1a normal challenge and hard challenge

---
+++ module-1a-hard/Program.cs
@@ -0,0 +1,109 @@
+﻿// MODULE 1A CHALLENGE
+// Design a text-based interaction with a shop that sells at least 3 items.
+// Each item has a price and a quantity. Present these options to you, the consumer, as a numbered list.
+// You have a "wallet" with some amount of money. Make sure you have enough money to buy!
+// Present a nice printout at the end detailing the transaction, and what's in your inventory.
+
+// things the player has
+int wallet = 11450;
+int[] playerQuantities = { 0, 0, 0, 0 };
+
+// shop item quantities
+string[] items = { "Sword", "Shield", "Leather Boots", "Bracelet" };
+int[] itemPrices = { 4500, 1000, 500, 1200 };
+int[] shopQuantities = { 3, 1, 2, 5 };
+
+
+while (true)
+{
+    Console.WriteLine(" ");
+    Console.WriteLine("###");
+    Console.WriteLine("Welcome to the Armory! What would you like to buy?");
+
+    Console.WriteLine(" ");
+    Console.WriteLine($"You have {wallet} munny.");
+
+    Console.WriteLine(" ");
+    Console.WriteLine("We have the following items in stock:");
+
+    // notice that the array index is just 1 less than the "choice" number we want!
+    for (int i = 0; i < items.Length; i++)
+    {
+        Console.WriteLine($"{i + 1}    {items[i]}, {itemPrices[i]} munny. In Stock: {shopQuantities[i]}");
+    }
+    
+    // we can also append new lines using a "carriage return" and "new line"
+    Console.WriteLine("5    Leave the shop\r\n");
+
+    string answer = Console.ReadLine();
+
+    int answerNumber = 0;
+
+    // TryParse makes our code more fool-proof - it won't throw an exception if the value is bad
+    bool result = int.TryParse(answer, out answerNumber);
+
+    // we can use this "actual index" as the index to check in the arrays
+    int actualIndex = answerNumber - 1;
+
+    // if we receive a 5 then we leave the store
+    if (answerNumber == 5)
+    {
+        break;
+    }
+    else
+    {
+        // if we receive anything else, we should check to see if it's valid
+        // two ways it can fail:
+        // 1) if TryParse fails to get an int from the string input
+        // 2) if the index is outside the bounds of the array
+
+        if (!result || actualIndex < 0 || actualIndex >= items.Length)
+        {
+            Console.WriteLine("Not a valid option!");
+            continue;
+        }
+    }
+
+    int itemCost = itemPrices[actualIndex];
+
+    // if we have enough money, we can buy it!
+    if (wallet >= itemCost)
+    {
+
+        // if the shop has enough stock, we can buy it!
+        if (shopQuantities[actualIndex] >= 1)
+        {
+            wallet -= itemCost;
+
+            // add to the player inventory
+            playerQuantities[actualIndex]++;
+
+            // subtract from the shop's inventory
+            shopQuantities[actualIndex]--;
+
+            // print out a summary of the transaction
+            Console.WriteLine(" ");
+            Console.WriteLine($"You just bought a {items[actualIndex]} for {itemPrices[actualIndex]} munny!");
+
+            Console.WriteLine($"You now have {wallet} munny.");
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Your inventory:");
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine($"You have {playerQuantities[i]} {items[i]}.");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, we are all out of {items[actualIndex]}!");
+        }
+    }
+    else
+    {
+        Console.WriteLine(" ");
+        Console.WriteLine("You gotta buy something or get out, pal!");
+    }
+}
+
+Console.WriteLine("Bye now!");
---
+++ module-1a/Program.cs
@@ -0,0 +1,110 @@
+﻿// MODULE 1A CHALLENGE
+// Design a text-based interaction with a shop that sells at least 3 items.
+// Each item has a price and a quantity. Present these options to you, the consumer, as a numbered list.
+// You have a "wallet" with some amount of money. Make sure you have enough money to buy!
+// Present a nice printout at the end detailing the transaction, and what's in your inventory.
+
+// this solution is not a good one. it does not use loops or arrays
+// the hard solution shows how powerful these programming features are!
+
+// things the player has
+int wallet = 11450;
+int playerSwords = 0;
+int playerShields = 0;
+int playerLeatherBoots = 0;
+int playerCharmedBracelet = 0;
+
+// shop item quantities
+int swordInStock = 3;
+int shieldInStock = 1;
+int leatherBootsInStock = 2;
+int charmedBraceletInStock = 5;
+
+Console.WriteLine(" ");
+Console.WriteLine("###");
+Console.WriteLine("Welcome to the Armory! What would you like to buy?");
+
+Console.WriteLine(" ");
+Console.WriteLine($"You have {wallet} munny.");
+
+Console.WriteLine(" ");
+Console.WriteLine("We have the following items in stock:");
+
+Console.WriteLine(" ");
+Console.WriteLine($"1   Sword, 4500 munny. In Stock: {swordInStock}");
+Console.WriteLine($"2   Shield, 1000 munny.In Stock: {shieldInStock}");
+Console.WriteLine($"3   Leather Boots, 500 money. In Stock: {leatherBootsInStock}");
+Console.WriteLine($"4   Charmed Bracelet, 1200 money. In Stock: {charmedBraceletInStock}");
+
+Console.WriteLine(" ");
+string answer = Console.ReadLine();
+
+int answerNumber = int.Parse(answer);
+
+int itemCost = 0;
+if (answerNumber == 1) itemCost = 4500;
+else if (answerNumber == 2) itemCost = 1000;
+else if (answerNumber == 3) itemCost = 500;
+else if (answerNumber == 4) itemCost = 1200;
+
+// if we have enough money, we can buy it!
+if (wallet >= itemCost)
+{
+    // notice the code duplication here... how can we make this better?
+    // start thinking about loops and arrays - we haven't covered them yet though!
+    if (answerNumber == 1)
+    {
+        // need to check the stock first
+        if (swordInStock >= 1)
+        {
+            wallet -= itemCost;
+            swordInStock--;
+            playerSwords++;
+        }
+    }
+    else if (answerNumber == 2)
+    {
+        if (shieldInStock >= 1)
+        {
+            wallet -= itemCost;
+            shieldInStock--;
+            playerShields++;
+        }
+    }
+    else if (answerNumber == 3)
+    {
+        if (leatherBootsInStock >= 1)
+        {
+            wallet -= itemCost;
+            leatherBootsInStock--;
+            playerLeatherBoots++;
+        }
+    }
+    else if (answerNumber == 4)
+    {
+        if (charmedBraceletInStock >= 1)
+        {
+            wallet -= itemCost;
+            charmedBraceletInStock--;
+            playerCharmedBracelet++;
+        }
+    }
+
+    Console.WriteLine(" ");
+    Console.WriteLine($"Purchase successful! You now have {wallet} munny.");
+
+    Console.WriteLine(" ");
+    Console.WriteLine("Your inventory:");
+    // think about this problem: how can we format this to have
+    // an "s" on everything except a quantity of 1?
+    Console.WriteLine($"You have {playerSwords} swords.");
+    Console.WriteLine($"You have {playerShields} shields.");
+    Console.WriteLine($"You have {playerLeatherBoots} leather boots.");
+    Console.WriteLine($"You have {playerCharmedBracelet} charmed bracelets.");
+}
+else
+{
+    Console.WriteLine("You gotta buy something or get out, pal!");
+}
+
+// notice this just runs once... how could we run this many times?
